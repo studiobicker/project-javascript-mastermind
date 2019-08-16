74e4c5da4203da616f53bd7c8057d1358eda9d55
Anne-Marie de Haan <anne@studiobicker.nl>
2019-08-16 07:44:01 +0200

    done some game logic

--- js/main.js
+++ js/main.js
@@ -1,27 +1,119 @@
 const colors = ["red", "blue", "green", "orange", "purple", "pink"];
-let mastermindGame = new MastermindGame(colors);
+const mastermindGame = new MastermindGame(colors);
+mastermindGame.generateSecretCode();
 let guess = [];
 
 $(document).ready(function() {
+  //temporary show the secret code in the footer
+  $("footer p").html(mastermindGame.secretCode);
+
+  //Setup the gameboard
+  setupGameBoard();
 
   $(".color-pegs .oval").click(function(){
-    const $this = $(this);
-    setColor($this);
-  
-    if ( guess.length > 3 ) {
-      console.log(guess);
-      debugger;
+
+    setColor($(this));
+
+    //debugger;
+    if ( isReadyForCheck(guess) ) {
+      //activate check-button
+      $(".btn").addClass("btn-check");
+
+      $(".btn-check").click(function(){
+        //de-activate checkbutton
+        $(this).removeClass("btn-check");
+        $(this).off();
+
+        //check the score of the guess
+        const scoreResult = mastermindGame.scoreGuess(guess);
+        const scoreOvals = $(".active .score .oval");
+
+        //display scores
+        for (let i=0;i<scoreResult.length;i++) {
+          $(scoreOvals[i]).addClass(scoreResult[i]);
+        }
+
+        //de-activate the row and activate the next row
+        activateRow($(`.row:eq(${mastermindGame.nextRow})`), $(`.row:eq(${mastermindGame.nextRow - 1})`));
+
+        //empty the guess array before the next guess
+        guess = [];
+      });
     }
 
   })
 
 });
 
+const setupGameBoard = () => {
+  let html = '';
+  for (let i=0;i<10;i++) {
+    html += '<div class="row">';
+    html += '<div class="pattern">';
+    for (let j=0;j<4;j++) {
+      html += '<div class="oval"></div>';
+    }
+    html += '</div>';
+    html += '<div class="score">';
+    for (let j=0;j<4;j++) {
+      html += '<div class="small oval"></div>';
+    }
+    html += '</div>';
+    html += '</div>';
+  }
+  $('#game-board').html(html);
+  activateRow($(`.row:eq(${mastermindGame.nextRow})`));
+}
+
+const activateRow = (row, prow)=> {
+  //remove class and event handlers if there is a previous row
+  if (prow) {
+    prow.removeClass("active");
+    prow.find(".pattern .oval").off();
+  } 
+
+  //add class to row and first oval in the row
+  row.addClass("active");
+  row.find(".pattern .oval").first().addClass("selected");
+
+  //add event handlers to the ovals in the active row
+  row.find(".pattern .oval").click(function(){
+    clearAndSelect($(this));
+  });
+}
+
 const setColor = color => {
-  const colorPeg = color.data("color");
-  guess.push(colorPeg);
+  const colorPeg = color.data("color");/* 
+  const arrayOfOvals = [...$(".active .pattern .oval")];
+
+  for (let i=0;i<arrayOfOvals;i++) {
+    if (arrayOfOvals[i].hasClass("selected")) {
+      debugger;
+      guess[i] = colorPeg
+    }
+  } */
 
   $(".active .selected").addClass(colorPeg);
-  $(".active .selected").removeClass("selected").next().addClass("selected");
+  guess.push(colorPeg);
+  // const listClass = color.class()
 
+  $(".active .selected").removeClass("selected").next().addClass("selected");
+}
+
+const clearAndSelect = oval => {
+  //remove selected class from prev/next oval
+  oval.parent().find(".selected").removeClass("selected");
+  //remove all classes from current oval and add class oval and selected
+  oval.removeClass().addClass("oval selected");
+}
+
+const isReadyForCheck = (guessArray) => {
+  guessArray.forEach(function(color) {
+    //debugger;
+    console.log(color);
+    if (!color) {
+      return false;
+    }   
+  });
+  return;
 }
--- js/mastermind.js
+++ js/mastermind.js
@@ -2,16 +2,31 @@ class MastermindGame {
   constructor(colors) {
     this.colors = colors;
     this.secretCode = [];
-    this.guess =[];
     this.numRows = 10;
+    this.nextRow = 0;
+
   }
 
   generateSecretCode() {
-
+    //randomly choose 4 colors from the six available colors
+    for (let i=0;i < 4;i++) {
+      this.secretCode[i] = this.colors[Math.floor( Math.random() * this.colors.length )];
+    }
   }
 
-  checkIfGuess(){
-
+  scoreGuess(guess) {
+    this.nextRow++
+    const score = [];
+    for (let i=0;i<guess.length;i++) {
+      if (this.secretCode.indexOf(guess[i]) === -1 ) {
+        score[i] = null;
+      } else if (this.secretCode[i] === guess[i]) {
+        score[i] = "great";
+      } else {
+        score[i] = "good";
+      }
+    }
+    return score.sort();
   }
 
   isFinished(){
