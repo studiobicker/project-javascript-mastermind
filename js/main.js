dd7da290fc0b59573383a9415608e2d9a6bfecd0
Anne-Marie de Haan <anne@studiobicker.nl>
2019-08-17 16:04:28 +0200

    basic version using tables

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 const colors = ["red", "blue", "green", "orange", "purple", "pink"];
 const mastermindGame = new MastermindGame(colors);
 mastermindGame.generateSecretCode();
-let guess = [];
+let guess = [null, null, null, null];
 
 $(document).ready(function() {
   //temporary show the secret code in the footer
@@ -9,111 +9,124 @@ $(document).ready(function() {
 
   //Setup the gameboard
   setupGameBoard();
+  const tableRow = $("table tr:first");
+  //Activate the first row
+  activateRow(tableRow);
 
   $(".color-pegs .oval").click(function(){
 
     setColor($(this));
-
-    //debugger;
-    if ( isReadyForCheck(guess) ) {
-      //activate check-button
-      $(".btn").addClass("btn-check");
-
-      $(".btn-check").click(function(){
-        //de-activate checkbutton
-        $(this).removeClass("btn-check");
-        $(this).off();
-
-        //check the score of the guess
-        const scoreResult = mastermindGame.scoreGuess(guess);
-        const scoreOvals = $(".active .score .oval");
-
-        //display scores
-        for (let i=0;i<scoreResult.length;i++) {
-          $(scoreOvals[i]).addClass(scoreResult[i]);
-        }
-
-        //de-activate the row and activate the next row
-        activateRow($(`.row:eq(${mastermindGame.nextRow})`), $(`.row:eq(${mastermindGame.nextRow - 1})`));
-
-        //empty the guess array before the next guess
-        guess = [];
-      });
-    }
-
   })
+
+  $(".btn-check").click(function(){
+
+    if ( isReadyForCheck(guess) ) {
+      //check the score of the guess
+      const scoreResult = mastermindGame.scoreGuess(guess);
+      const scoreOvals = $(".active .score .oval");
+
+      //display scores
+      for (let i=0;i<scoreResult.length;i++) {
+        $(scoreOvals[i]).addClass(scoreResult[i]);
+      }
+
+      setTimeout(function(){
+        const currentRow = $(".active");
+        const nextRow = currentRow.closest('tr').next('tr');
+        //check if game is finished
+        if (mastermindGame.isFinished()) {
+          deActivateRow(currentRow);
+          $(".btn-check, .color-pegs .oval").off();
+          alert('game over');
+        } else {
+          //de-activate the row and activate the next row
+          deActivateRow(currentRow);
+          activateRow(nextRow);
+          //empty the guess array before the next guess
+          guess = [null,null,null,null];
+        }
+      }, 100);
+
+
+    }
+  });  
 
 });
 
 const setupGameBoard = () => {
   let html = '';
+  html += '<table class="board">';
   for (let i=0;i<10;i++) {
-    html += '<div class="row">';
-    html += '<div class="pattern">';
+    html += '<tr>';
+    html += '<td class="ind"></td>';
+    html += '<td class="input"><table><tr>';
     for (let j=0;j<4;j++) {
-      html += '<div class="oval"></div>';
+      html += '<td><div class="oval"></div></td>';
     }
-    html += '</div>';
-    html += '<div class="score">';
-    for (let j=0;j<4;j++) {
-      html += '<div class="small oval"></div>';
+    html += '</tr></table></td>';
+    html += '<td class="score">';
+    html += '<table>';
+    for (let j=0;j<2;j++) {
+      html += '<tr>'
+      for (let k=0;k<2;k++) {
+        html += '<td><div class="small oval"></div></td>';
+      }
+      html += '</tr>'
     }
-    html += '</div>';
-    html += '</div>';
+    html += '</table></td>';
   }
+  html += '</table>';
   $('#game-board').html(html);
-  activateRow($(`.row:eq(${mastermindGame.nextRow})`));
+
 }
 
-const activateRow = (row, prow)=> {
-  //remove class and event handlers if there is a previous row
-  if (prow) {
-    prow.removeClass("active");
-    prow.find(".pattern .oval").off();
-  } 
-
-  //add class to row and first oval in the row
+const activateRow = row => {
+  //add class active to row and class selected to first cell in table inside active row 
   row.addClass("active");
-  row.find(".pattern .oval").first().addClass("selected");
+  row.find(".input table td:first").addClass("selected");
 
-  //add event handlers to the ovals in the active row
-  row.find(".pattern .oval").click(function(){
+  //add event listeners to table cells of active rows
+  row.find(".input table td").click(function(){
     clearAndSelect($(this));
   });
 }
 
-const setColor = color => {
-  const colorPeg = color.data("color");/* 
-  const arrayOfOvals = [...$(".active .pattern .oval")];
-
-  for (let i=0;i<arrayOfOvals;i++) {
-    if (arrayOfOvals[i].hasClass("selected")) {
-      debugger;
-      guess[i] = colorPeg
-    }
-  } */
-
-  $(".active .selected").addClass(colorPeg);
-  guess.push(colorPeg);
-  // const listClass = color.class()
-
-  $(".active .selected").removeClass("selected").next().addClass("selected");
+const deActivateRow = row => {
+  //remove class and event listeners from tablerow
+  row.removeClass("active");
+  row.find(".input table td").off();
 }
 
-const clearAndSelect = oval => {
-  //remove selected class from prev/next oval
-  oval.parent().find(".selected").removeClass("selected");
-  //remove all classes from current oval and add class oval and selected
-  oval.removeClass().addClass("oval selected");
+const setColor = color => {
+  const colorPeg = color.data("color");
+  const tableCell = $(".active .selected");
+  const columnNr = tableCell[0].cellIndex;
+  const tableRow = tableCell.parent();
+
+  $(".active .selected").addClass(colorPeg).removeClass("selected");
+  guess[columnNr] = colorPeg;
+
+  // add class 'selected' to first empty tableCell
+  tableRow.find("td:not([class]):first").addClass("selected");
+}
+
+const clearAndSelect = tableCell => {
+  const columnNr = tableCell[0].cellIndex;
+
+  //remove value from guessArray
+  guess[columnNr] = null;
+  //remove class 'selected' from other tableCell
+  tableCell.parent().find("td.selected").removeAttr("class");
+  //remove color from current tableCell and add class selected to current tableCell
+  tableCell.removeClass().addClass("selected");
+
 }
 
 const isReadyForCheck = (guessArray) => {
-  guessArray.forEach(function(color) {
-    //debugger;
-    console.log(color);
+  for (let color of guessArray) {
     if (!color) {
       return false;
-    }   
-  });
-  return;
+    }
+  }
+  return true;
 }
--- js/mastermind.js
+++ js/mastermind.js
@@ -2,6 +2,7 @@ class MastermindGame {
   constructor(colors) {
     this.colors = colors;
     this.secretCode = [];
+    this.numCorrect = 0;
     this.numRows = 10;
     this.nextRow = 0;
 
@@ -17,21 +18,72 @@ class MastermindGame {
   scoreGuess(guess) {
     this.nextRow++
     const score = [];
+    let scoreCorrect = 0;
     for (let i=0;i<guess.length;i++) {
       if (this.secretCode.indexOf(guess[i]) === -1 ) {
         score[i] = null;
       } else if (this.secretCode[i] === guess[i]) {
-        score[i] = "great";
+        score[i] = "bothgood";
+        scoreCorrect++;
       } else {
-        score[i] = "good";
+        score[i] = "colorgood";
       }
     }
+    this.numCorrect = scoreCorrect;
     return score.sort();
   }
 
   isFinished(){
+    if(this.numRows === this.nextRow || this.numCorrect === 4) {
+      return true;
+    } else {
+      return false;
+    }
+  } 
+}
 
+class Chronometer {
+  constructor(show) {
+    this.currentTime = 0;
+    this.intervalId = null;
+    this.showTime = show
+  }
+  
+  startClick() {
+    this.intervalId = setInterval(()=>{
+      //debugger;
+      this.currentTime++
+      this.showTime(...this.setTime())
+    },1000);
+  }
+  
+  getMinutes() {
+    const minutes = Math.floor(this.currentTime / 60);
+    return minutes;
+  }
+  
+  getSeconds() {
+    const seconds = this.currentTime - this.getMinutes() * 60;
+    return seconds;
+  }
+  
+  twoDigitsNumber(aNum) {
+    const twoDigits = aNum.toString().length > 1 ? aNum : "0"+aNum;
+    return twoDigits;
   }
 
+  setTime() {
+    const minutes = this.twoDigitsNumber(this.getMinutes());
+    const seconds = this.twoDigitsNumber(this.getSeconds());
+    return [minutes,seconds];
+  }
+
+  stopClick() {
+    clearInterval(this.intervalId);
+  }
+  
+  resetClick() {
+    this.currentTime = 0;
+  }
 
 }
